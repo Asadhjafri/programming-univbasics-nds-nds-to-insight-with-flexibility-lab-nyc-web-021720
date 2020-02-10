d38b0617c7b5bb12ffc34a85c3505103d9ca4031
Asad Jafri <asadhjafri@gmail.com>
2020-02-10 03:25:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,7 +37,7 @@ def movies_with_director_key(name, movies_collection)
   result = []
   i = 0
 
-  while i < movies.collection.length do
+  while i < movies_collection.length do
     movie_data = movies_collection[i]
     result << movie_with_director_name(name, movie_data)
     i += 1
